68c4bba1b43354432aa93ef430f5d75ef5b9cb87
Yoan Ante <yoanante@gmail.com>
2019-01-11 11:53:36 -0500

    added authorization header and moved to new folder

---
+++ src/adapters/ApiAdapter.js
@@ -0,0 +1,45 @@
+
+class JSONAPIAdapter {
+  constructor(endpoint) {
+    this.endpoint = `http://localhost:3000/${endpoint}`
+    this.headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+    }
+  }
+  getAll() {
+    return fetch(this.endpoint)
+      .then(response => response.json())
+  }
+  getSingle(id) {
+    return fetch(`${this.endpoint}/${id}`)
+      .then(response => response.json())
+  }
+  // { key: 'value', key: { key: 'value', key: 'value'} }
+  createItem(body) {
+    return fetch(this.endpoint, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(body)// { key: 'value', key: { key: 'value', key: 'value'} }
+    })
+  }
+
+  deleteItem(id) {
+    return fetch(`${this.endpoint}/${id}`, {
+      method: 'DELETE',
+      header: this.headers
+    }).then(response => response.json())
+  }
+
+  updateItem(body, id) {
+    return fetch(`${this.endpoint}/${id}`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify(body)
+    })
+  }
+}
+
+export default JSONAPIAdapter;
+
