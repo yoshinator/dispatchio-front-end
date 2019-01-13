2ec84b367215721bc287168b230b18ebaafbf2f6
Yoan Ante <yoanante@gmail.com>
2019-01-13 00:40:28 -0500

    added new types for jobs actions

--- src/reducers/types.js
+++ src/reducers/types.js
@@ -1,14 +1,22 @@
 
-export const ADD_COMPANY    = "ADD_COMPANY"
-export const ADD_LOCATION   = "ADD_LOCATION"
-export const ADD_CUSTOMER   = "ADD_CUSTOMER"
+export const ADD_COMPANY    = "ADD_COMPANY";
+export const ADD_LOCATION   = "ADD_LOCATION";
+export const ADD_CUSTOMER   = "ADD_CUSTOMER";
 
 
 
-// LOGIN REDUCER TYPES
+// LOGIN REDUCER 
 export const ADD_USER             = "ADD_USER";
 export const SET_CURRENT_USER     = "SET_CURRENT_USER";
 export const AUTHENTICATING_USER  = "AUTHENTICATING_USER";
 export const AUTHENTICATED_USER   = "AUTHENTICATED_USER";
 export const FAILED_LOGIN         = "FAILED_LOGIN";
-export const LOG_OUT              = "LOG_OUT"
+export const LOG_OUT              = "LOG_OUT";
+
+//JOB REDUCER 
+export const CHANGE_STATUS  = "CHANGE_STATUS";
+export const ADD_NOTE       = "ADD_NOTE";
+
+//JOBS REDUCER 
+export const GET_JOBS       = "GET_JOBS";
+export const ADD_JOB        = "ADD_JOB"
