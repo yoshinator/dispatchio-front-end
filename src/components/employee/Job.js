fcf7bf13620d7bc11e8acbfc1c3dcd6ec0d2d5aa
Yoan Ante <yoanante@gmail.com>
2019-01-13 23:36:41 -0500

    employee can update job and update will have his time stamp

--- src/components/employee/Job.js
+++ src/components/employee/Job.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 
+import JobUpdater from "./JobUpdater"
+
 
 class Job extends Component {
 
   jsxBuilder = () => {
     if (this.props.jobs){
+      
     return this.props.jobs.map(job => {
-      return(
-           < div key={job.id} className = "card" >
-            <div className="card-header" id="headingOne">
-              <h2 className="mb-0">
-                <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  {job.city.toUpperCase()} | {job.customer.name.toUpperCase()}
-                  </button>
-              </h2>
-            </div>
+      console.log("JOB DESCRIPTION", job.description);
+      return <div className="card" key={job.id}>
+          <div className="card-header" id={`heading${job.id}`}>
+            <h2 className="mb-0">
+              <button className="btn btn-link" type="button" data-toggle="collapse" data-target={`#collapse${job.id}`} aria-expanded="true" aria-controls={`collapse${job.id}`}>
+                {job.status} {job.customer.name} {job.customer.phone} {job.city}
+              </button>
+            </h2>
+          </div>
 
-      <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-        <div className="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
-      </div>
-      </div >
-      )
+     
+          <div id={`collapse${job.id}`} className="collapse  hide" aria-labelledby={`heading${job.id}`} data-parent="#accordionExample">
+            <div className="card-body">
+            <span>
+              <i className="fas fa-globe-americas"> </i><a target="_blank" rel="noopener noreferrer" href={`http://maps.google.com/maps?q=${job.street_1},${job.street_2},+${job.city},+${job.state}+${job.zip}`}>
+                {`${job.street_1} ${job.street_2}, ${job.city}, ${job.state} ${job.zip} `}
+              </a>{" "}
+            </span>
+            <span>Description: {job.description}</span>
+              <JobUpdater job={job} />
+            </div>
+          </div>
+        </div>;
     })
     }
     else {
@@ -42,6 +51,7 @@ class Job extends Component {
   }
 
   render() {
+    
   return this.jsxBuilder()
   }
 }
---
+++ src/components/employee/JobUpdater.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+import { getJobsAction } from '../../actions/job'
+import JSONAPIAdapter from '../../adapters/ApiAdapter'
+const JOBAdapter = new JSONAPIAdapter("api/v1/jobs")
+
+class JobUpdater extends Component {
+  state ={
+    status: "IR in route",
+    description: ""
+    }
+
+  handleChange =(event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  getCurrentFormattedTime=()=>{
+    return new Date().toLocaleString("en-US", {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: "true"
+    })
+  }
+
+  //THIS IS WHERE WE NEED UPDATE THE DB
+  success = (pos) => {
+    const crd = pos.coords;
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    const time = this.getCurrentFormattedTime()
+    const delimiter = "\n-------------------\n"
+    // Builds the body with status as the current status and description as the previous description plus the description that is being added with a time stamp and employee name.
+    const body = { status: this.state.status, 
+      description: this.props.description + delimiter + " \nUpdated at: " + time + " \n "+ "By: "+ this.props.user.f_name + " " + this.props.user.l_name + this.state.description +delimiter}
+    JOBAdapter.updateItem(body, this.props.job.id)
+    .then(response => console.log(response))
+  }
+
+  //UPDATE HERE ALSO INCASE OF NAVIGATOR ISSUE
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    navigator.geolocation.getCurrentPosition(this.success, this.error);
+    console.log(this.state)
+  }
+  render() {
+    console.log(this.props)
+    return <div className="card inner-card">
+        <div className="card-body">
+          <form onSubmit={this.handleSubmit} autoComplete="one">
+            <div className="form-group">
+              <label htmlFor="jobStatusSelect">Change Status</label>
+              <select onChange={this.handleChange}className="form-control" name="status" id={`jobStatusSelect${this.props.job.id}`}>
+                <option>IR in route</option>
+                <option>IP in progress</option>
+                <option>DN done</option>
+                <option>FU follow up</option>
+              </select>
+            </div>
+            <div className="form-group">
+            <label htmlFor={`add-to-description${this.props.job.id}`}>Add to description</label>
+            <textarea onChange={this.handleChange} name="description" className="form-control" id={`add-to-description${this.props.job.id}`} rows="2" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Update
+            </button>
+          </form>
+        </div>
+      </div>;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.loginReducer.user
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getJobs: (day, id) => {
+      dispatch(getJobsAction(day, id))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobUpdater)
