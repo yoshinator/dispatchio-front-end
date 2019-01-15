dcac4b50356158ca0c8e75f79e93078363cba863
Yoan Ante <yoanante@gmail.com>
2019-01-15 08:55:53 -0500

    Added getJobsAction for all jobs not just employee jobs

--- src/actions/job.js
+++ src/actions/job.js
@@ -1,10 +1,11 @@
 import JSONAPIAdapter from '../adapters/ApiAdapter'
 const JOBAdapter = new JSONAPIAdapter("api/v1/jobs/user_date");
+const JOBSAdapter = new JSONAPIAdapter("api/v1/jobs/date");
 
 
 
 
-export const getJobsAction = (day, id) => {
+export const getEmployeeJobsAction = (day, id) => {
   console.log("job Action getJobsAction")
   return (dispatch) => {
 
@@ -33,4 +34,33 @@ export const getJobsAction = (day, id) => {
   }
 }
 
+export const getJobsAction = (day) => {
 
+  return (dispatch) => {
+
+    console.log("IN ACTION CREATOR")
+    // I WOULD LIKE TO PUT A SPINNER HERE OF SOMEKIND WHILE WE LOAD.
+    dispatch({ type: 'AUTHENTICATING USER' })
+
+  const body = {
+    "job": {
+      "schedule_date": day
+    }
+  }
+
+  JOBSAdapter.createItem(body)
+    .then(response => {
+      if (response.ok) {
+        console.log("THE RESPONSE", response)
+        return response.json()
+      } else {
+        throw response
+      }
+    })
+    .then(JSONResponse => {
+      console.log(JSONResponse)
+      dispatch({ type: "GET_JOBS", payload: JSONResponse })
+    })
+  }
+
+}
