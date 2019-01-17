2e2615687ecbaa498afd05aa54225617c5f82ec3
Yoan Ante <yoanante@gmail.com>
2019-01-17 14:21:56 -0500

    jobs reducer fixed issues where the state was being changed incorrectly"

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -17,11 +17,12 @@ const jobReducer = (state = initialState, action) => {
       //jobs gets overwritten with action.payload which should be all the jobs in a 7 day period.
     case GET_WEEK:
       return  {jobs: [...initialState.jobs, ...action.payload], editingJob: {...state.editingJob}}
-      //jobs array keeps old state editingJob gets the value of a selected jobObject 
+      //jobs array keeps old state editingJob gets the value of a selected jobObject which is what renders to editjob form
     case EDIT_JOB:
-      return { jobs: [...[], ...state.jobs], editingJob: action.payload }
+      return { jobs: [...state.jobs], editingJob: action.payload }
+      //replaces the updated job in our state's jobs array.
     case UPDATE_JOB:
-      return { ...initialState, ...action.payload }
+       return { jobs: state.jobs.filter(job => job.id !== action.payload.id).concat(action.payload), editingJob: initialState.editingJob}
     default:
       return state;
   }
