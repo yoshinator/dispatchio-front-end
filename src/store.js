1271a2c3b9f6f2dd4a6c004eb850d509d326f3fa
Yoan Ante <yoanante@gmail.com>
2019-01-17 22:10:54 -0500

    code cleanup deletion of unsused imports

--- src/store.js
+++ src/store.js
@@ -4,17 +4,13 @@ import thunk from "redux-thunk";
 
 import loginReducer from './reducers/loginReducer';
 import companyReducer from './reducers/companyReducer';
-// import weekViewReducer from "./reducers/weekViewReducer";
-// import jobsReducer from  './reducers/jobsReducer'
 import jobReducer from  './reducers/jobReducer'
 import teamReducer from './reducers/teamReducer'
 import teamMemberReducer from './reducers/teamMemberReducer'
 
 const rootReducer = combineReducers({
   companyReducer,
-  // jobsReducer,
   loginReducer,
-  // weekViewReducer,
   jobReducer,
   teamReducer,
   teamMemberReducer
