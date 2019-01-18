f40958c8d3ca474f530093f3334acb71c1109d37
Yoan Ante <yoanante@gmail.com>
2019-01-17 22:33:24 -0500

    removed deprecated function call.

--- src/components/employee/JobsSearch.js
+++ src/components/employee/JobsSearch.js
@@ -5,26 +5,28 @@ import Jobs from './Jobs'
 import YOANHelpers from '../../helpers/helpers'
 const timeHelper = new YOANHelpers();
 
-class JobsSearch extends React.Component{
+
+// THIS COMPONENT ONLY DISPLAYS FOR USERS OF TYPE EMPLOYEE
+class JobsSearch extends React.Component {
   state = {
-    date: "",
-  }
+    date: ""
+  };
 
   componentDidMount() {
-    this.props.getJobs(timeHelper.getDay(), this.props.user.id)
+    this.props.getJobs(timeHelper.getDay(), this.props.user.id);
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(this.state.date)
-    console.log(this.props.user.id, this.props.getJobs)
-    this.props.getJobs(this.state.date, this.props.user.id)
-  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.date);
+    console.log(this.props.user.id, this.props.getJobs);
+    this.props.getJobs(this.state.date, this.props.user.id);
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const dateArray = event.target.value.split("-");
     const year = dateArray[0];
-    const month = parseInt(dateArray[1]) -1;
+    const month = parseInt(dateArray[1]) - 1;
     const date = dateArray[2];
     const _entryDate = new Date(year, month, date);
     this.setState({
@@ -33,24 +35,32 @@ class JobsSearch extends React.Component{
         day: "numeric",
         year: "numeric"
       })
-    })
-  }
+    });
+  };
 
-
+  // THIS COMPONENT ONLY DISPLAYS FOR USERS OF TYPE EMPLOYEE
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <>
         <h2 className="justify-content-center d-flex p-2 ">JOB SEARCH</h2>
         <div className="justify-content-center d-flex p-2 ">
-        <form className="justify-content-center d-flex p-2" onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="date" name="date" min={timeHelper.dateTransform(timeHelper.getFormattedDay())}/>
-          <button>Submit</button>
-        </form>
-      </div>
+          <form
+            className="justify-content-center d-flex p-2"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              onChange={this.handleChange}
+              type="date"
+              name="date"
+              min={timeHelper.dateTransform(timeHelper.getDay())}
+            />
+            <button>Submit</button>
+          </form>
+        </div>
         <Jobs />
       </>
-      )
+    );
   }
 }
 
