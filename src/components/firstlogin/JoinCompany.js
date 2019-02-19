8914da993d7c3840ef65d21157e225a936340b3b
Yoan Ante <yoanante@gmail.com>
2019-02-19 10:23:30 -0500

    employees or managers that log in for first time will see find your company screen

--- src/components/firstlogin/JoinCompany.js
+++ src/components/firstlogin/JoinCompany.js
@@ -1,12 +1,43 @@
-
+//JOIN COMPANY CONTROLLER FOR MANAGERS AND EMPLOYEES NOT FOR OWNERS
 import React, { Component } from 'react'
 import { Redirect } from 'react-router'
 import { connect } from 'react-redux'
+import { getCompaniesAction } from '../../actions/companies'
 
 class JoinCompany extends Component {
 
+
+  state = {
+    companySearch: "",
+    foundCompanies: [],
+    chosenCompany: {},
+    foundLocations: [],
+    chosenLocation: {}
+  }
+
   componentDidMount(){
-    console.log("Hello")
+    this.props.getCompanies()
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }, this.findCompanies)
+
+  }
+
+  findCompanies = () => {
+    this.setState({
+      foundCompanies: this.props.companies.filter(company => {
+        return company.name.toLowerCase().includes(this.state.companySearch.toLowerCase())
+      }) 
+    }) 
+  }
+
+  displayCompanies = () => {
+   return this.state.foundCompanies.map(company => {
+     return <option key={company.id} value={company.id}>{company.name}</option>
+   })
   }
 
   render() {
@@ -17,17 +48,34 @@ class JoinCompany extends Component {
 
       return (
         <div>
-          <h1>In Join Company</h1>
+          <h1>Find Your Company</h1>
+
+            <div className="form-group">
+              <label htmlFor="companySearch">Type to start searching for your company</label>
+              <input onChange={this.handleChange} className="form-control" name="companySearch" id="companySearch"></input>
+            </div>
+          <select>{this.displayCompanies()}</select>
+
+
         </div>
       )
   }
 }
 
 
-const mapsStateToProps = ({loginReducer: {user}}) => {
+const mapsStateToProps = ({loginReducer: {user}, companiesReducer: {companies}}) => {
   return {
-    user
+    user, companies
   }
 }
 
-export default connect(mapsStateToProps)(JoinCompany)
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getCompanies: () => {
+      dispatch(getCompaniesAction())
+    }
+  };
+}
+
+export default connect(mapsStateToProps, mapDispatchToProps)(JoinCompany)
