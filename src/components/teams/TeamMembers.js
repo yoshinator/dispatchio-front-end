3d5c7d8387c8e650031a0079d18134aa2e354637
yoshinator <yoanante@gmail.com>
2020-05-12 23:28:49 -0400

    a little light late night refactoring

--- src/components/teams/TeamMembers.js
+++ src/components/teams/TeamMembers.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getTeamMembersAction, setTeamMemberAction, changeTeamMemberEditFlagAction, createNewTeamMemberFlagAction} from '../../actions/team';
 import withAuth from '../../hocs/withAuth'
 import withRoleManager from '../../hocs/withRoleManager'
 
-class TeamMembers extends Component {
+const TeamMembers = (props) =>  {
 
-  componentDidMount () {
-    this.props.getTeamMembers(this.props.user.location.id);
-  }
-  handleClick =(teamMember) => {
-    this.props.setTeamMemberAction(teamMember)
-  }
-  handleEdit = (teamMember) => {
-    this.props.setTeamMemberAction(teamMember);
-    this.props.changeTeamMemberEditFlag();
 
-  }
+  props.getTeamMembers(props.user.location.id);
 
-  createNewTeamMember = () => {
-    this.props.createNewTeamMemberFlag();
+  const handleClick =(teamMember) => {
+    props.setTeamMemberAction(teamMember)
+  }
+  const handleEdit = (teamMember) => {
+    props.setTeamMemberAction(teamMember);
+    props.changeTeamMemberEditFlag();
+
   }
 
-  teamMembersJsx = () => {
+  const createNewTeamMember = () => {
+    props.createNewTeamMemberFlag();
+  }
 
-    if (this.props.teamMembers.team_members && this.props.teamMembers.team_members.length > 0) {
-      return this.props.teamMembers.team_members.map(teamMember => {
+  const teamMembersJsx = () => {
+
+    if (props.teamMembers.team_members && props.teamMembers.team_members.length > 0) {
+      return props.teamMembers.team_members.map(teamMember => {
         return <div key={Date.now()*Math.random()}>
             {" "}
             <p className= "card-content" key={teamMember.id}>
-              {teamMember.f_name} {teamMember.l_name} <span onClick={() => this.handleClick(teamMember)}>
+              {teamMember.f_name} {teamMember.l_name} <span onClick={() => handleClick(teamMember)}>
                 {" "}
               <Link to={`/map/${teamMember.id}`}><i className="fas fa-map-marker-alt"></i></Link>
               </span>
-              <button className="button" onClick={() => this.handleEdit(teamMember)} type="button">
+              <button className="button" onClick={() => handleEdit(teamMember)} type="button">
                 {" "}
                 Edit{" "}
               </button>
@@ -44,40 +44,38 @@ class TeamMembers extends Component {
     }
   }
 
-  renderTeamMembers = () => {
+  const renderTeamMembers = () => {
     return (
         <div>
           <h2>
             Team Members
           </h2>
-          {this.teamMembersJsx()}
+          {teamMembersJsx()}
         </div>
    
     )
   }
 
-  render() {
-
-    if (!this.props.teamMembers.teamMemberEditFlag && !this.props.teamMembers.createTeamMemberFlag) {
-      return (
-          <main className="container">
-            <div className="form-container">
-              <div>{this.renderTeamMembers()}</div>
-            </div>
-          <button className="button" onClick={this.createNewTeamMember} >
-            <span>Add Team Member</span>{" "}
-          </button>
-          </main>
-      )
-    } else if (this.props.teamMembers.createTeamMemberFlag) { 
-      return <Redirect to="/createteammember"></Redirect>
-    }
-      else {return <Redirect to="/editteammember"></Redirect>
-    }
+  if (!props.teamMembers.teamMemberEditFlag && !props.teamMembers.createTeamMemberFlag) {
+    return (
+        <main className="container">
+          <div className="form-container">
+            <div>{renderTeamMembers()}</div>
+          </div>
+        <button className="button" onClick={createNewTeamMember} >
+          <span>Add Team Member</span>{" "}
+        </button>
+        </main>
+    )
+  } else if (props.teamMembers.createTeamMemberFlag) { 
+    return <Redirect to="/createteammember"></Redirect>
   }
+    else {return <Redirect to="/editteammember"></Redirect>
+  }
+
 } 
 
-// PLEASE DESTRUCTURE THE STATE SO I DON'T HAVE TO THIS. `this.props.teamMembers.team_members`
+// PLEASE DESTRUCTURE THE STATE SO I DON'T HAVE TO  `props.teamMembers.team_members`
 const mapStateToProps = (state) => ({
   user: state.loginReducer.user,
   teamMembers: state.teamMemberReducer
